2e87fb4122c07f60a98b6fabb367868a45a48250
lzh <247437853@qq.com>
2017-06-14 11:17:54 +0800

    https://github.com/chinalee1990/FlashScrollDemo

---
+++ MyFlash/FlashNoAutoScroll.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace MyFlash
+{
+    public partial class FlashNoAutoScroll
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FlashNoAutoScroll));
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.m_Flash = new AxShockwaveFlashObjects.AxShockwaveFlash();
+            ((System.ComponentModel.ISupportInitialize)(this.m_Flash)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.vScrollBar1.Location = new System.Drawing.Point(835, 0);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(17, 344);
+            this.vScrollBar1.TabIndex = 0;
+            this.vScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar1_Scroll);
+            this.vScrollBar1.SizeChanged += new System.EventHandler(this.vScrollBar1_SizeChanged);
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.hScrollBar1.Location = new System.Drawing.Point(0, 327);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(835, 17);
+            this.hScrollBar1.TabIndex = 1;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
+            this.hScrollBar1.SizeChanged += new System.EventHandler(this.hScrollBar1_SizeChanged);
+            // 
+            // m_Flash
+            // 
+            this.m_Flash.Enabled = true;
+            this.m_Flash.Location = new System.Drawing.Point(0, 0);
+            this.m_Flash.Name = "m_Flash";
+            this.m_Flash.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("m_Flash.OcxState")));
+            this.m_Flash.Size = new System.Drawing.Size(832, 324);
+            this.m_Flash.TabIndex = 2;
+            this.m_Flash.SizeChanged += new System.EventHandler(this.m_Flash_SizeChanged);
+            // 
+            // FlashNoAutoScroll
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.vScrollBar1);
+            this.Controls.Add(this.m_Flash);
+            this.Name = "FlashNoAutoScroll";
+            this.Size = new System.Drawing.Size(852, 344);
+            ((System.ComponentModel.ISupportInitialize)(this.m_Flash)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        public AxShockwaveFlashObjects.AxShockwaveFlash m_Flash;
+
+    }
+}
---
+++ MyFlash/FlashNoAutoScroll.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MyFlash
+{
+    public partial class FlashNoAutoScroll: UserControl
+    {
+        public FlashNoAutoScroll()
+        {
+            InitializeComponent();
+            hScrollBar1.Minimum = 0;
+            vScrollBar1.Minimum = 0;
+            SetScrollBar_H();
+            SetScrollBar_V();
+        }
+
+        public void SetFlashSize(int nWidth,int nHeight)
+        {
+            m_Flash.Width = nWidth;
+            m_Flash.Height = nHeight;
+        }
+
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            //Flash水平滚动
+            if (hScrollBar1.Value < 0)
+                return;
+            m_Flash.Location = new Point(-hScrollBar1.Value, m_Flash.Location.Y);
+        }
+
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            //Flash垂直滚动
+            if (vScrollBar1.Value < 0)
+                return;
+            m_Flash.Location = new Point(m_Flash.Location.X, -vScrollBar1.Value);
+        }
+
+
+        private void m_Flash_SizeChanged(object sender, EventArgs e)
+        {
+            SetScrollBar_H();
+            SetScrollBar_V();
+        }
+
+        private void hScrollBar1_SizeChanged(object sender, EventArgs e)
+        {
+            //SetScrollBar_H();
+        }
+
+        private void vScrollBar1_SizeChanged(object sender, EventArgs e)
+        {
+            //SetScrollBar_V();
+        }
+
+        void SetScrollBar_H()
+        {
+            hScrollBar1.Maximum = m_Flash.Width - hScrollBar1.Width;
+        }
+
+        void SetScrollBar_V()
+        {
+            vScrollBar1.Maximum = m_Flash.Height - vScrollBar1.Height;
+        }
+
+
+
+    }
+}
